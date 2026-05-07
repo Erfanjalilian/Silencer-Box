b624beaa5edf494430a9619fe697d8de726b8060
Erfan <erfanjaliliyan83@gmail.com>
2026-05-06 23:43:06 -0700

    Creating the special discounts component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Hero from "./components/Hero";
 import Articles from "./components/Articles";
 import BestSellingProducts from "./components/BestSellingProducts";
+import HighestDiscounts from "./components/HighestDiscounts";
 export default function Home() {
   return (
     <div>
       <Hero/>
       <Articles/>
       <BestSellingProducts/>
+      <HighestDiscounts/>
     </div>
   );
 }
